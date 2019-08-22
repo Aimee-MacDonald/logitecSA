a601bd5c9131fa9d8a1cc1547851d9b266c16986
Aimee MacDonald <aimeelmacdonald@gmail.com>
2019-08-22 11:37:50 +0200

    Quote Request Section

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const bodyParser = require("body-parser");
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "/views"));
 
 app.use(express.static(path.join(__dirname, "/static")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get("/", (req, res) => {
   res.status(200).render("index");
@@ -13,6 +18,11 @@ app.get("/", (req, res) => {
 
 app.get("/todo", (req, res) => {
   res.status(200).render("todo");
+});
+
+app.post("/quote", (req, res) => {
+  console.log("Quote Request:");
+  console.log(req.body);
 });
 
 app.listen(process.env.PORT || 8080);
